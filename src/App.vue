81074fd5c682dcc2f374a05b1116dc75ed64f6de
Craig Huang <chuang25@vt.edu>
2024-11-21 00:56:16 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <v-app class="app">
+    <RouterView />
+  </v-app>
+</template>
+
+<style scoped lang="sass">
+.app
+  position: absolute
+  top: 0
+  left: 0
+  right: 0
+  bottom: 0
+</style>
